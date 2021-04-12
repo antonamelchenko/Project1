e589d6eba603f83a1f206caa0871f9fb35bcbc91
antonamelchenko@inbox.ru <antonamelchenko@inbox.ru>
2021-04-12 12:32:56 +0300

    Добавьте файлы проекта.

---
+++ Project1/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿
+namespace Project1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.scoreLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 25;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // scoreLabel
+            // 
+            this.scoreLabel.AutoSize = true;
+            this.scoreLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.scoreLabel.Location = new System.Drawing.Point(40, 25);
+            this.scoreLabel.Name = "scoreLabel";
+            this.scoreLabel.Size = new System.Drawing.Size(48, 52);
+            this.scoreLabel.TabIndex = 0;
+            this.scoreLabel.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Project1.Resource1._11;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1135, 890);
+            this.Controls.Add(this.scoreLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label scoreLabel;
+    }
+}
+
---
+++ Project1/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project1
+{
+    public partial class Form1 : Form
+    {
+
+        public Bitmap HandlerTexture = Resource1.Handler;
+        public Bitmap TargetTexture = Resource1.Target;
+
+        private Point _targetPosition = new Point(300, 300);
+        private Point _direction = Point.Empty;
+        private int _score = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            //Убираем блики
+            SetStyle(ControlStyles.OptimizedDoubleBuffer |
+                ControlStyles.AllPaintingInWmPaint |
+                ControlStyles.UserPaint, true);
+
+            UpdateStyles();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+
+            var localPosition = this.PointToClient(Cursor.Position);
+
+            _targetPosition.X += _direction.X * 7;
+            _targetPosition.Y += _direction.Y * 7;
+
+            if (_targetPosition.X < 0 || _targetPosition.X > 500)
+            {
+                _direction.X *= -1;
+            }
+
+            if (_targetPosition.Y < 0 || _targetPosition.Y > 500)
+            {
+                _direction.Y *= -1;
+            }
+
+            Point between = new Point(localPosition.X - _targetPosition.X, localPosition.Y - _targetPosition.Y);
+            float distance = (float)Math.Sqrt((between.X * between.X) + (between.Y * between.Y));
+
+            if (distance < 20)
+            {
+                AddScore(1);
+            }
+
+            var handlerRect = new Rectangle(localPosition.X - 50, localPosition.Y - 50, 100, 100);
+            var targetRect = new Rectangle(_targetPosition.X - 50, _targetPosition.Y - 50, 100, 100);
+
+            g.DrawImage(HandlerTexture, handlerRect);
+            g.DrawImage(TargetTexture, targetRect);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Refresh();
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            Random r = new Random();
+            timer2.Interval = r.Next(25, 500);
+            _direction.X = r.Next(-1, 2);
+            _direction.Y = r.Next(-1, 2);
+        }
+
+        private void AddScore(int score)
+        {
+            _score += score;
+            scoreLabel.Text = _score.ToString();
+        }
+    }
+}
